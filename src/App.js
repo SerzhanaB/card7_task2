3610a00292bd46e6b119c3d78185f95b7db8debd
Serzhana <buraevase@gmail.com>
2021-09-16 21:10:57 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let a = "";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { massive: ["dklkdf1"] };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleValue = this.handleValue.bind(this);
+  }
+
+  handleValue = (event) => {
+    this.setState(({ massive }) => {
+      const newValue = event.target.value;
+      // return {massive: [...massive,newValue]} ;
+      console.log("on change ", newValue);
+
+      a = newValue;
+      //console.log("on change a ", a)
+      return a;
+    });
+  };
+
+  handleClick = () => {
+    this.setState((massive) => {
+      const res = a;
+      console.log("on button", res);
+
+      return { massive: [...massive, res] };
+    });
+  };
+
+  /* 
+press() {
+
+  var className =(this.state.class==="off") ? "on": "off"; 
+  this.setState({class:className});
+    
+*/
+
+  render() {
+    return (
+      <>
+        <ul>
+          {this.state.massive.map((item, index) => (
+            <li key={index.toString()}>
+              {item} <button>Сделать зачеркнутым </button>
+            </li>
+          ))}
+        </ul>
+        <input onChange={this.handleValue} />
+        <br />
+        <button onClick={this.handleClick}>Добавить элемент </button>
+        <br />
+        <button>Удалить последний элемент </button>
+      </>
+    );
+  }
 }
-
 export default App;
